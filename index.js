ee5e095929be609c2f549b03a27be7aa065bb77a
Sam Peck <fullstacksam@protonmail.com>
2023-05-03 14:40:55 -0400

    first time it worked

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", cool);
+
+
+
+
+console.log("heyyyy")
+
+
+
+
+function cool (event){
+    
+    let pToChange = document.getElementById('text')
+    pToChange.innerHTML=`This is really cool!`;
+
+};
